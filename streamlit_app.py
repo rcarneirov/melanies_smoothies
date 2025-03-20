364409b77fabf010e34bc0e353ddfe13ab9a584b
rcarneirov <63800326+rcarneirov@users.noreply.github.com>
2025-03-20 14:00:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ helpful_links = [
 ]
 
 # Write directly to the app
-st.title("Customize your Smoothie Melanie :cup_with_straw:")
+st.title("Customize your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 
